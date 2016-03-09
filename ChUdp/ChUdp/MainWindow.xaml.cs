87a97f1e41cdc12db6b7297289c4763e87504c11
NextLight <samuele.turci13@gmail.com>
2016-03-09 11:45:27 +0100

    Implemented Sock class. Dunno if it werks

--- ChUdp/ChUdp/MainWindow.xaml.cs
+++ ChUdp/ChUdp/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChUdp
 {
@@ -20,7 +13,7 @@ namespace ChUdp
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        Sock sock;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,7 +21,7 @@ namespace ChUdp
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-
+            sock = new Sock();
         }
     }
 }
---
+++ ChUdp/ChUdp/Sock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChUdp
+{
+    class Sock
+    {
+        Socket socket;
+        // 12345 is broadcast
+        public Sock(int port = 12345)
+        {
+            socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+            socket.Connect(new IPEndPoint(IPAddress.Broadcast, port));
+        }
+
+        public void SendBytes(byte[] buffer)
+        {
+            socket.Send(buffer);
+        }
+
+        public void SendString(string s)
+        {
+            SendBytes(Encoding.UTF8.GetBytes(s));
+        }
+
+        public async Task<Tuple<byte[], IPAddress>> ReceiveBytes()
+        {
+            byte[] buffer = new byte[socket.Available];
+            EndPoint sender = new IPEndPoint(IPAddress.Any, 1234);
+            await Task.Run(() => socket.ReceiveFrom(buffer, ref sender));
+            return new Tuple<byte[], IPAddress>(buffer, ((IPEndPoint)sender).Address);
+        }
+
+        public async Task<Tuple<string, IPAddress>> ReceiveString()
+        {
+            var t = await ReceiveBytes();
+            return new Tuple<string, IPAddress>(Encoding.UTF8.GetString(t.Item1), t.Item2);
+        }
+    }
+}
